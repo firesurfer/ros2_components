51cfccd0d1bf61e241a7be463e35e2c96b09191d
firesurfer <firesurfer65@yahoo.de>
2017-02-17 16:42:35 +0100

    Removed old tests and added missing license information for files

--- src/tests/create_and_rebuild_robot.cpp
+++
@@ -1,8 +0,0 @@
-#include "rclcpp/rclcpp.hpp"
-
-rclcpp::node::Node::SharedPtr node;
-int main(int argc, char** argv)
-{
-    rclcpp::init(argc, argv);
-    node = rclcpp::node::Node::make_shared("create_and_rebuild_robot_test");
-}
