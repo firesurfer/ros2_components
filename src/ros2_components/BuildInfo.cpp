80afb3616f61e2fc59b691d2d6660f75b14747bf
Lennart Nachtigall <firesurfer127@gmail.com>
2018-04-21 21:29:03 +0200

    enhanced build info

--- src/ros2_components/BuildInfo.cpp
+++ src/ros2_components/BuildInfo.cpp
@@ -1,1 +1,3 @@
 #include "BuildInfo.h"
+
+std::vector<std::string> ros2_components::BuildInfo::libraries_names ;
--- src/ros2_components/BuildInfo.h
+++ src/ros2_components/BuildInfo.h
@@ -1,6 +1,11 @@
 #ifndef BUILDINFO_H
 #define BUILDINFO_H
 #include <string>
+#include <limits.h>
+#include <unistd.h>
+#include <link.h>
+#include <vector>
+#include "Reflect.h"
 namespace  ros2_components {
 
 /**
@@ -25,7 +30,38 @@ public:
 #define BUILD_VERSION "not set"
 #endif
         return std::string(BUILD_VERSION);
+    }
 
+    static std::string get_exe_path()
+    {
+        char result[ PATH_MAX ];
+        ssize_t count = readlink( "/proc/self/exe", result, PATH_MAX );
+        return std::string( result, (count > 0) ? count : 0 );
+    }
+    static std::string get_current_directory()
+    {
+        char* cur_dir_ptr = get_current_dir_name();
+        std::string current_dir_name = std::string(cur_dir_ptr);
+        std::free(cur_dir_ptr);
+        return current_dir_name;
+    }
+    static std::vector<std::string> get_loaded_libraries()
+    {
+        libraries_names.clear();
+        dl_iterate_phdr(iterate_libraries,NULL);
+        return BuildInfo::libraries_names;
+    }
+private:
+    static std::vector<std::string> libraries_names;
+
+    static int iterate_libraries(struct dl_phdr_info *info, size_t size, void *data)
+    {
+        UNUSED(size);
+        UNUSED(data);
+        std::string name = std::string(info->dlpi_name);
+
+        BuildInfo::libraries_names.push_back(name);
+        return 0;
     }
 };
 }
